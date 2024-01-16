5ce19ef92fb11af10c5c73d416000b8df5ab9223
nhestrompia <nhestrompia@gmail.com>
2024-01-16 10:53:15 +0300

    fix: deploy errors

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -20,7 +20,19 @@ export default function FAQ() {
                 How to play Super Leo Lig?
               </AccordionTrigger>
               <AccordionContent className="text-black/80">
-              <p>You can play <strong>Super Leo Lig</strong> by connecting your Aleo account using the Puzzle Wallet. Download the wallet <a href="https://chromewebstore.google.com/detail/puzzle-wallet/fdchdcpieegfofnofhgdombfckhbcokj" target="_blank">here</a>.</p>              </AccordionContent>
+                <p>
+                  You can play <strong>Super Leo Lig</strong> by connecting your
+                  Aleo account using the Puzzle Wallet. Download the wallet{" "}
+                  <a
+                    href="https://chromewebstore.google.com/detail/puzzle-wallet/fdchdcpieegfofnofhgdombfckhbcokj"
+                    target="_blank"
+                    className="underline"
+                  >
+                    here
+                  </a>
+                  .
+                </p>{" "}
+              </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
               <AccordionTrigger className="hover:no-underline font-semibold tracking-tight">
@@ -28,7 +40,7 @@ export default function FAQ() {
               </AccordionTrigger>
               <AccordionContent className="text-black/80">
                 Two players can play against each other after selecting their
-                teams and entering the wagers. 
+                teams and entering the wagers.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
@@ -44,7 +56,9 @@ export default function FAQ() {
                 Why is Super Leo Lig build on Aleo?
               </AccordionTrigger>
               <AccordionContent className="text-black/80">
-                Super Leo Lig is only available on Aleo, because Aleo enables apps that are private by default. This enables users to commit to their strategy, without revealing it to the opponent. 
+                Super Leo Lig is only available on Aleo, because Aleo enables
+                apps that are private by default. This enables users to commit
+                to their strategy, without revealing it to the opponent.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-5">
@@ -52,7 +66,8 @@ export default function FAQ() {
                 Am I able to buy/sell players for my team?
               </AccordionTrigger>
               <AccordionContent className="text-black/80">
-                In the future it will be possible. Players will be tokenized as NFT's and tradeable on the market.
+                In the future it will be possible. Players will be tokenized as
+                NFT&apos;s and tradeable on the market.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem className="border-b-0" value="item-6">
@@ -60,7 +75,12 @@ export default function FAQ() {
                 Will I get rugged if my opponent rage quits?
               </AccordionTrigger>
               <AccordionContent className="text-black/80">
-                No! Super Leo Lig uses the Puzzle SDK and wallet, which are optimized for multiparty privacy games. Because Super Leo lig implements the Puzzle SDK, all wagered amounts are stored in a time-locked multisig. If an opponent decides to not finish the game, the wagered amount is released to the user after the time-lock expires.
+                No! Super Leo Lig uses the Puzzle SDK and wallet, which are
+                optimized for multiparty privacy games. Because Super Leo lig
+                implements the Puzzle SDK, all wagered amounts are stored in a
+                time-locked multisig. If an opponent decides to not finish the
+                game, the wagered amount is released to the user after the
+                time-lock expires.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
