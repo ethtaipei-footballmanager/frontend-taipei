31641b8dc6bdb071967e8ddafabb8dadfd5ee213
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-03-07 21:53:11 +0800

    update fees

--- src/app/state/manager.ts
+++ src/app/state/manager.ts
@@ -16,7 +16,7 @@ export const GAME_FUNCTIONS = {
 export const transitionFees = {
   propose_game: 0.027,
   submit_wager: 0.0286,
-  accept_game: 0.04901,
+  accept_game: 0.05901,
   calculate_outcome: 0.1,
   reveal_answer: 0.1,
   finish_game: 0.1,
